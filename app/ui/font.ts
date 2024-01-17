07f9323ffd90c0e35c0a9417d0966a0c213ff340
우영경 <yg.woo@piolink.com>
2024-01-17 16:30:57 +0900

    폰트수정

--- app/ui/font.ts
+++ app/ui/font.ts
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google';
 import { Noto_Sans } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const sans = Noto_Sans({ subsets: ['latin'], weight: '300' });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
