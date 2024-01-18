7165080ec2d429c07fb1a5130149c69203c2c287
우영경 <yg.woo@piolink.com>
2024-01-18 17:56:05 +0900

    챕터11~

--- app/test/component/Greeting.tsx
+++ app/test/component/Greeting.tsx
@@ -7,10 +7,10 @@ const Greeting = () => {
 
   return (
     <>
-      <div className="mt-9 flex items-center gap-2 bg-yellow-200 p-4">
-        <div className="bg-pink-200">가나다</div>
-        <div className="bg-pink-200">라마바</div>
-        <div className="shrink-0 bg-pink-200 px-10">사아자</div>
+      <div className="mt-9 flex items-end gap-2 bg-yellow-200 p-4">
+        <div className="bg-pink-200">안녕하</div>
+        <div className="bg-pink-200">세요</div>
+        <div className="shrink-0 bg-pink-200 px-10">환영합니다</div>
       </div>
       <button onClick={() => router.push('/')}>HOME</button>
     </>
