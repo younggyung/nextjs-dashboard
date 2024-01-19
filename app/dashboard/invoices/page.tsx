2f94d379a6d4cdb52bad82bb189eaff803bd732f
우영경 <yg.woo@piolink.com>
2024-01-19 18:05:53 +0900

    Search 파트

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,7 +6,16 @@ import { lusitana } from '@/app/ui/font';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 
-export default async function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -14,11 +23,11 @@ export default async function Page() {
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search invoices..." />
-        {/* <CreateInvoice /> */}
+        <CreateInvoice />
       </div>
-      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
-      </Suspense> */}
+      </Suspense>
       <div className="mt-5 flex w-full justify-center">
         {/* <Pagination totalPages={totalPages} /> */}
       </div>
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,8 +1,12 @@
 const TestPage = () => {
   return (
     <>
-      <div className="bg-sky-200">
-        <p>Page Area</p>
+      <div className="relative h-[200px] w-[400px] bg-sky-200">
+        최상위 디브
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-red-100">
+          top과 left를 50% 씩 주면 왼쪽 상단 꼭짓점이 중앙에 위치, translate으로
+          위치보정
+        </div>
       </div>
     </>
   );
